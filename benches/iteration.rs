78d2f24d49150491acadbcbf7c9f5b5d11676433
Andrew Aylett <andrew@aylett.co.uk>
2021-09-05 14:33:35 +0100

    Another iai benchmark

--- benches/iteration.rs
+++ benches/iteration.rs
@@ -1,5 +1,7 @@
+use anyhow::Error;
 use iai::{black_box, main};
 use mandelbrot::complex::{Complex, Fix2x61, FixResult};
+use mandelbrot::set::Set;
 
 const ZERO: Complex = Complex::new(Fix2x61::zero(), Fix2x61::zero());
 
@@ -17,4 +19,15 @@ fn iai_benchmark_zero() -> FixResult<Complex> {
     black_box(ZERO).iterate_mandelbrot(&black_box(ZERO))
 }
 
-main!(iai_benchmark_zero, iai_benchmark_zero_five_times);
+fn iai_benchmark_full_set_render() -> Result<Set, Error> {
+    let centre = Complex::new(Fix2x61::zero(), Fix2x61::zero());
+    let radius = Fix2x61::two();
+
+    Set::create(7, black_box(centre), black_box(radius))?.iterate_as_required(20)
+}
+
+main!(
+    iai_benchmark_zero,
+    iai_benchmark_zero_five_times,
+    iai_benchmark_full_set_render
+);
