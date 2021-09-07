8e5a5ff4fb998c61f46be94ff657d551954407f8
Andrew Aylett <andrew@aylett.co.uk>
2021-09-07 20:55:00 +0100

    Put schemes in a trait for easier implementation
    I should wrap that up in a macro sometime

--- src/colours.rs
+++ src/colours.rs
@@ -4,13 +4,90 @@ use std::path::Path;
 use std::str::FromStr;
 
 use anyhow::{bail, Error};
+use image::ColorType;
+use std::ops::Deref;
 
-pub struct RGB(pub u8, pub u8, pub u8);
+pub struct Rgb(pub u8, pub u8, pub u8);
+
+impl From<&Rgb> for Vec<u8> {
+    fn from(r: &Rgb) -> Self {
+        vec![r.0, r.1, r.2]
+    }
+}
 
 #[derive(Debug, Copy, Clone)]
 pub enum ColourScheme {
     Greyscale,
     Fractint,
+    LogGreyscale,
+}
+
+struct Greyscale;
+static GREYSCALE: Greyscale = Greyscale {};
+struct Fractint;
+static FRACTINT: Fractint = Fractint {};
+struct LogGreyscale;
+static LOGGREYSCALE: LogGreyscale = LogGreyscale {};
+
+pub trait ColourSchemeT {
+    fn colour_type(&self) -> image::ColorType;
+    fn bytes(&self, iterations: u64) -> Vec<u8>;
+    fn escaped_bytes(&self) -> Vec<u8>;
+}
+
+impl Deref for ColourScheme {
+    type Target = dyn ColourSchemeT;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            ColourScheme::Greyscale => &GREYSCALE,
+            ColourScheme::Fractint => &FRACTINT,
+            ColourScheme::LogGreyscale => &LOGGREYSCALE,
+        }
+    }
+}
+
+impl ColourSchemeT for Greyscale {
+    fn colour_type(&self) -> ColorType {
+        image::ColorType::Gray(8)
+    }
+
+    fn bytes(&self, iterations: u64) -> Vec<u8> {
+        vec![((iterations % 255) + 1) as u8]
+    }
+
+    fn escaped_bytes(&self) -> Vec<u8> {
+        vec![0]
+    }
+}
+impl ColourSchemeT for Fractint {
+    fn colour_type(&self) -> ColorType {
+        image::ColorType::RGB(8)
+    }
+
+    fn bytes(&self, iterations: u64) -> Vec<u8> {
+        let modulus = VGA_MAP.len();
+        let c = &VGA_MAP[iterations as usize % modulus];
+        c.into()
+    }
+
+    fn escaped_bytes(&self) -> Vec<u8> {
+        vec![0, 0, 0]
+    }
+}
+impl ColourSchemeT for LogGreyscale {
+    fn colour_type(&self) -> ColorType {
+        image::ColorType::Gray(8)
+    }
+
+    fn bytes(&self, iterations: u64) -> Vec<u8> {
+        let log_iter = ((iterations as f64).log2() * 64f64) as usize;
+        vec![((log_iter % 255) + 1) as u8]
+    }
+
+    fn escaped_bytes(&self) -> Vec<u8> {
+        vec![0]
+    }
 }
 
 impl FromStr for ColourScheme {
@@ -20,40 +97,9 @@ impl FromStr for ColourScheme {
         Ok(match s {
             "fractint" => ColourScheme::Fractint,
             "grey" => ColourScheme::Greyscale,
+            "loggrey" => ColourScheme::LogGreyscale,
             _ => bail!("Invalid colour scheme"),
         })
-    }
-}
-
-impl ColourScheme {
-    fn colour_type(&self) -> image::ColorType {
-        match &self {
-            ColourScheme::Greyscale => image::ColorType::Gray(8),
-            ColourScheme::Fractint => image::ColorType::RGB(8),
-        }
-    }
-
-    fn bytes(&self, iterations: u64) -> Vec<u8> {
-        match &self {
-            ColourScheme::Greyscale => {
-                vec![((iterations % 255) + 1) as u8]
-            }
-            ColourScheme::Fractint => {
-                let c = &VGA_MAP[(iterations % 16) as usize];
-                vec![c.0, c.1, c.2]
-            }
-        }
-    }
-
-    fn escaped_bytes(&self) -> Vec<u8> {
-        match &self {
-            ColourScheme::Greyscale => {
-                vec![0]
-            }
-            ColourScheme::Fractint => {
-                vec![0, 0, 0]
-            }
-        }
     }
 }
 
@@ -83,22 +129,22 @@ impl Set {
 
 lazy_static! {
     // Ref https://svn.fractint.net/trunk/fractint/maps/default.map
-    pub static ref VGA_MAP: Vec<RGB> = vec!(
-        RGB(0, 0, 0),
-        RGB(0, 0, 168),
-        RGB(0, 168, 0),
-        RGB(0, 168, 168),
-        RGB(168, 0, 0),
-        RGB(168, 0, 168),
-        RGB(168, 84, 0),
-        RGB(168, 168, 168),
-        RGB(84, 84, 84),
-        RGB(84, 84, 252),
-        RGB(84, 252, 84),
-        RGB(84, 252, 252),
-        RGB(252, 84, 84),
-        RGB(252, 84, 252),
-        RGB(252, 252, 84),
-        RGB(252, 252, 252),
+    pub static ref VGA_MAP: Vec<Rgb> = vec!(
+        Rgb(0, 0, 0),
+        Rgb(0, 0, 168),
+        Rgb(0, 168, 0),
+        Rgb(0, 168, 168),
+        Rgb(168, 0, 0),
+        Rgb(168, 0, 168),
+        Rgb(168, 84, 0),
+        Rgb(168, 168, 168),
+        Rgb(84, 84, 84),
+        Rgb(84, 84, 252),
+        Rgb(84, 252, 84),
+        Rgb(84, 252, 252),
+        Rgb(252, 84, 84),
+        Rgb(252, 84, 252),
+        Rgb(252, 252, 84),
+        Rgb(252, 252, 252),
     );
 }
